6bd2a5ddd4006060daf5bb3a4cf49de30a57d441
VitaliRodionovski <vitalijrodionovskij5@gmail.com>
2023-02-26 16:21:28 +0400

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         logiNTester MainTester = new logiNTester();
-        boolean TrueLogin = MainTester.isLoginValid("Starboy98");
+        boolean TrueLogin = MainTester.isLoginValid("starboy98");
         if (TrueLogin) System.out.println("Login is suitable");
         else System.out.println("Login does not meet requirements");
     }
